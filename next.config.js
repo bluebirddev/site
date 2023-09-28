e3bb1648374524b0f722433096fd8f627f4e8ca5
CJ <cj@bluebird.dev>
2023-09-28 19:40:23 +0400

    Optimize main bg image

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: false,
+})
+module.exports = withBundleAnalyzer({})
